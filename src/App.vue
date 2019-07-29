59b33591e182f2144381ab12d0a76a7c1a247276
Edmore Musvibe <emusvibe007@gmail.com>
2019-07-29 02:54:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Test
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="customers container">
+  <h1 class="page-header">About</h1>
+  <p>This is a leads manager app</p>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
